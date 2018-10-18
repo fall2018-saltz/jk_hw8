685124fae8a4e49b730bf425db63160d71b5ddf9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:34:53 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,7 +3,7 @@ library(rjson)
 #Step A: Load the data
 
 #Reading the JSON data into json variable
-json<-fromJSON('hotelSurveySherison.json')
+json<-fromJSON(file = 'hotelSurveySherison.json')
 #converting the data from json to dataframe mydata
 mydata<-data.frame(json$data,stringsAsFactors = FALSE)
 mydata
