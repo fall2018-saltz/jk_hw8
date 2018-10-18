ac73d7704d46b721da7a9f8ebe1c2a044e8a0184
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:21:18 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -4,7 +4,7 @@ library(jsonlite)
 #Step A: Load the data
 
 #Reading the JSON data into json variable
-json<-fromJSON("https://blackboard.syracuse.edu/bbcswebdav/pid-5159146-dt-content-rid-38224657_1/courses/21341.1191/hotelSurveySherison.json", flatten=TRUE)
+json<-fromJSON("hotelSurveySherison.json", flatten=TRUE)
 #converting the data from json to dataframe mydata
 mydata<-data.frame(json$data,stringsAsFactors = FALSE)
 mydata
