5140b6249f5be098ffb632ef27b3538b6277d845
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:14:25 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -8,3 +8,4 @@ library(dplyr)
 json<-fromJSON("https://blackboard.syracuse.edu/bbcswebdav/pid-5159146-dt-content-rid-38224657_1/courses/21341.1191/hotelSurveySherison.json", flatten=TRUE)
 #converting the data from json to dataframe mydata
 mydata<-data.frame(json$data,stringsAsFactors = FALSE)
+mydata
